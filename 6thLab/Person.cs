b6f2df1f44cc45f9e0621ae7b660f10e64c55e22
Андрей Зоммер <m2201132@edu.misis.ru>
2023-02-09 20:58:28 +0300

    Добавьте файлы проекта.

---
+++ 6thLab/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6thLab
+{
+    public struct Person
+    {
+        public string lastName;
+        public string society;
+        public double firstTry;
+        public double lastTry;
+
+        public readonly double TotalLength;
+
+        public Person(string lastName, string society, double firstTry, double lastTry)
+        {
+            this.lastName = lastName;
+            this.society = society;
+            this.firstTry = firstTry;
+            this.lastTry = lastTry;
+            TotalLength = firstTry + lastTry;
+        }
+    }
+}
---
+++ 6thLab/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6thLab
+{
+    internal class Program
+    {
+        static int seed = DateTime.Now.Millisecond;
+        static Random r = new Random(seed);
+
+        static void Main(string[] args)
+        {
+            Task1_1();
+            Console.ReadKey();
+        }
+
+        #region Level1
+        #region Task1
+        static void Task1_1()
+        {
+            int countParticipants = 0;
+            while(countParticipants == 0) 
+                int.TryParse(Console.ReadLine(), out countParticipants);
+
+            List<Person> participants = new List<Person>();
+            for(int i = 0; i < countParticipants; i++)
+            {
+                Console.Write("Enter the last name of participant: ");
+                string lastName = Console.ReadLine();
+                Console.Write("Enter the society: ");
+                string society = Console.ReadLine();
+                participants.Add(new Person(lastName, society, GetLength(), GetLength()));
+            }
+
+            participants.Sort(delegate (Person person1, Person person2)
+            {
+                return person1.TotalLength.CompareTo(person2.TotalLength) * (-1);
+            });
+
+            int place = 1;
+            foreach(var participant in participants)
+            {
+                Console.WriteLine($"place: {place}, last name: {participant.lastName}, society: {participant.society}, " +
+                    $"total length = {participant.TotalLength}, fist try = {participant.firstTry}, last try = {participant.lastTry}");
+                place++;
+            }
+        }
+
+        static double GetLength()
+        {
+            return (r.NextDouble() + 1) * 3;
+        }
+        #endregion
+        #endregion
+    }
+}
